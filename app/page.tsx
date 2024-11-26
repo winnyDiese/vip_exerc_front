e630902d4cca7a1cb84afbc461056a1b1447bd4b
winnyDiese <winnydiese@gmail.com>
2024-11-26 15:27:25 +0100

    lkjb

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from "react"
 
 const Home: React.FC = ()=> {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100"></div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Typescript</h1>
+    </div>
   )
 }
 
